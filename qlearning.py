b6757751a27f76a59bd1275c0d9a78570454b963
Omar Aflak <aflakomar@gmail.com>
2018-07-21 12:20:46 +0200

    added comments and min lr, min epsilon

--- qlearning.py
+++ qlearning.py
@@ -2,21 +2,27 @@ from time import sleep
 import numpy as np
 import gym
 
+# Environment
 env = gym.make('FrozenLake-v0')
 inputCount = env.observation_space.n
 actionsCount = env.action_space.n
 
+# Init Q-Table
 Q = {}
 for i in range(inputCount):
     Q[i] = np.random.rand(actionsCount)
 
+# Hyperparameters
 lr = 0.33
+lrMin = 0.001
 lrDecay = 0.9999
 gamma = 1.0
 epsilon = 1.0
+epsilonMin = 0.001
 epsilonDecay = 0.97
 episodes = 2000
 
+# Training
 for i in range(episodes):
     print("Episode {}/{}".format(i + 1, episodes))
     s = env.reset()
@@ -32,9 +38,10 @@ for i in range(episodes):
         Q[s][a] = Q[s][a] + lr * (r + gamma * np.max(Q[newS]) - Q[s][a])
         s = newS
 
-        lr *= lrDecay
+        if lr > lrMin:
+            lr *= lrDecay
 
-        if not r==0:
+        if not r==0 and epsilon > epsilonMin:
             epsilon *= epsilonDecay
 
 
@@ -42,7 +49,7 @@ print("")
 print("Learning Rate :", lr)
 print("Epsilon :", epsilon)
 
-# Play game on 100 episodes
+# Testing
 print("\nPlay Game on 100 episodes...")
 
 avg_r = 0
